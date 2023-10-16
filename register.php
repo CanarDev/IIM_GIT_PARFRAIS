e55d5c91e3d3c976fff3f2f43fd3eb59754f29b7
Enas <enas.elmershati@gmail.com>
2023-10-16 16:18:54 +0200

    :white_check_mark:Finalisation de l'inscription. Fix #2

--- register.php
+++ register.php
@@ -2,9 +2,34 @@
 require('config/config.php');
 require('model/functions.fn.php');
 
-/*===============================
-	Register
-===============================*/
+
+
+if(isset($_POST['email']) && isset($_POST['password'])  && isset($_POST['username'])){
+	if(!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['username'])){
+
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$username = $_POST['username'];
+
+
+		if(isUsernameAvailable($db, $username) == true){
+			
+			if(isEmailAvailable($db, $email) == true){
+
+				userRegistration($db, $username, $email, $password);
+				header('Location: login.php');
+
+			}
+			else{
+				$error = 'Email indisponible';
+			}
+		}
+		else{
+			$error = 'Username indisponible';
+		}
+		
+	}
+}
 
 include 'view/_header.php';
 include 'view/register.php';
