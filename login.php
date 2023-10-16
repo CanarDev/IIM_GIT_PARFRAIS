799b0949d2eafd3646e04c9a99a3baee9b2121e2
CanarDev <vincentmichelnavarro@gmail.com>
2023-10-16 13:13:36 +0200

    :white_check_mark: Connect with variable

--- login.php
+++ login.php
@@ -14,10 +14,11 @@ require('model/functions.fn.php');
 if(isset($_POST['email']) && isset($_POST['password'])){
 	if(!empty($_POST['email']) && !empty($_POST['password'])){
 
-		// TODO
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
 		// Force user connection to access dashboard
-		userConnection($db, 'git@initiation.com', 'password');
+		userConnection($db, $email, $password);
 		
 		header('Location: dashboard.php');
 
